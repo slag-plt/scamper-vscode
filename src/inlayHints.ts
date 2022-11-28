ca9f06a75b25f8dc7a6acac2657eb05683db9ffd
Oluwatobi Alabi <tobialabi609@gmail.com>
2022-11-28 10:31:06 -0600

    small edits

--- src/inlayHints.ts
+++ src/inlayHints.ts
@@ -35,7 +35,7 @@ async function getEvaluated(src: string, document: vscode.TextDocument): Promise
             const error = "error"
             const errors = holdState.range
             let position= new vscode.Position(errors.end.line, errors.end.column)
-            const label = [new vscode.InlayHintLabelPart( error)]
+            const label = [new vscode.InlayHintLabelPart(" --> "+ error)]
             const hint = new vscode.InlayHint(position, label)
             hint.paddingLeft = true
             output.push(hint)
@@ -53,7 +53,7 @@ async function getEvaluated(src: string, document: vscode.TextDocument): Promise
             const error = "error"
             const errors = holdState.range
             let position= new vscode.Position(errors.end.line, errors.end.column)
-            const label = [new vscode.InlayHintLabelPart( error)]
+            const label = [new vscode.InlayHintLabelPart( " --> "+error)]
             const hint = new vscode.InlayHint(position, label)
             hint.paddingLeft = true
             output.push(hint)
@@ -71,7 +71,7 @@ async function getEvaluated(src: string, document: vscode.TextDocument): Promise
             const error = "error"
             const errors = holdState.range
             let position= new vscode.Position(errors.end.line, errors.end.column)
-            const label = [new vscode.InlayHintLabelPart( error)]
+            const label = [new vscode.InlayHintLabelPart( " --> "+error)]
             const hint = new vscode.InlayHint(position, label)
             hint.paddingLeft = true
             output.push(hint)
@@ -92,7 +92,7 @@ async function getEvaluated(src: string, document: vscode.TextDocument): Promise
             const position = new vscode.Position(pos.line, column)//deal with offset in column
             holdState
             const expStr =  holdState.output? holdState.output : 'void'
-            const label = [new vscode.InlayHintLabelPart(expStr)]
+            const label = [new vscode.InlayHintLabelPart(" --> "+expStr)]
             const hint = new vscode.InlayHint(position, label)
             hint.paddingLeft = true
             output.push(hint)
@@ -102,7 +102,7 @@ async function getEvaluated(src: string, document: vscode.TextDocument): Promise
             const error = "error!!" // accessing the evaluated error statement leads to loss of all errors in the list i don't know why yet
             const errors = statement.range
             let position= new vscode.Position(errors.end.line, errors.end.column+1)
-            const label = [new vscode.InlayHintLabelPart( error)]
+            const label = [new vscode.InlayHintLabelPart(" --> "+ error)]
             const hint = new vscode.InlayHint(position, label)
             hint.paddingLeft = true
             output.push(hint)
@@ -111,7 +111,7 @@ async function getEvaluated(src: string, document: vscode.TextDocument): Promise
           default: {
             const expStr =  scamper.expToString(0, statement.value, false)
             const posH = src.indexOf(expStr) + expStr.length
-            const label = [new vscode.InlayHintLabelPart("exp not evaluated "+expStr)]
+            const label = [new vscode.InlayHintLabelPart(" --> "+"exp not evaluated "+expStr)]
             const hint = new vscode.InlayHint(document.positionAt(posH), label)
             hint.paddingLeft = true
             output.push(hint)
@@ -126,7 +126,7 @@ async function getEvaluated(src: string, document: vscode.TextDocument): Promise
             const pos = statement.actual.range.end
             const position = new vscode.Position(pos.line, pos.column)
             const bool = holdState.passed? "True" : "False"
-            const label = [new vscode.InlayHintLabelPart(bool)]
+            const label = [new vscode.InlayHintLabelPart(" --> "+bool)]
             const hint = new vscode.InlayHint(position, label)
             hint.paddingLeft = true
             output.push(hint)
@@ -136,7 +136,7 @@ async function getEvaluated(src: string, document: vscode.TextDocument): Promise
             const error = "error!!!"
             const errors = statement.range
             let position= new vscode.Position(errors.end.line, errors.end.column)
-            const label = [new vscode.InlayHintLabelPart( error)]
+            const label = [new vscode.InlayHintLabelPart(" --> "+ error)]
             const hint = new vscode.InlayHint(position, label)
             hint.paddingLeft = true
             output.push(hint)
